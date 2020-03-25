ff406442ef8923208ff7fa1ce81fd6e3cc6baac2
AbhijeetFadol <46525845+Abhifadol@users.noreply.github.com>
2020-03-25 19:30:58 +0530

    Create Week 2 Quiz.py

---
+++ Week 2 Quiz.py
@@ -0,0 +1,115 @@
+##
+
+1.Question 1
+Which of the following best describes "Regular Expressions"?
+
+(a) A way to calculate mathematical values paying attention to operator precedence
+(b) A small programming language unto itself
+(c) The way Python handles and recovers from errors that would otherwise cause a traceback
+(d) A way to solve Algebra formulas for the unknown value
+
+Ans :- A small programming language unto itself
+
+2.Question 2
+Which of the following is the way we match the "start of a line" in a regular expression?
+
+(a) ^
+(b) str.startswith()
+(c) \linestart
+(d) String.startsWith()
+(e) variable[0:1]
+
+Ans :- ^
+
+3.Question 3
+What would the following mean in a regular expression? [a-z0-9]
+
+(a) Match any number of lowercase letters followed by any number of digits
+(b) Match anything but a lowercase letter or digit
+(c) Match a lowercase letter or a digit
+(d) Match any text that is surrounded by square braces
+(e) Match an entire line as long as it is lowercase letters or digits
+
+Ans :- Match anything but a lowercase letter or digit
+
+4.Question 4
+What is the type of the return value of the re.findall() method?
+
+(a) An integer
+(b) A string
+(c) A boolean
+(d) A list of strings
+(e) A single character
+
+Ans :- A list of strings
+
+5.Question 5
+What is the "wild card" character in a regular expression (i.e., the character that matches any character)?
+
+(a) ^
+(b) .
+(c) *
+(d) ?
+(e) +
+(f) $
+
+Ans :- .
+
+6.Question 6
+What is the difference between the "+" and "*" character in regular expressions?
+
+(a) The "+" matches at least one character and the "*" matches zero or more characters
+(b) The "+" matches upper case characters and the "*" matches lowercase characters
+(c) The "+" matches the beginning of a line and the "*" matches the end of a line
+(d) The "+" matches the actual plus character and the "*" matches any character
+(f) The "+" indicates "start of extraction" and the "*" indicates the "end of extraction"
+
+Ans :- The "+" matches at least one character and the "*" matches zero or more characters
+
+7.Question 7
+What does the "[0-9]+" match in a regular expression?
+
+(a) One or more digits
+(b) Zero or more digits
+(c) Several digits followed by a plus sign
+(d) Any number of digits at the beginning of a line
+(f) Any mathematical expression
+
+Ans :- One or more digits
+
+8.Question 8
+What does the following Python sequence print out?
+
+(a) 
+(b) ['From: Using the :']
+(c) :
+(d) ['From:']
+(e) ^F.+:
+(f) From:
+
+Ans :- ['From: Using the :']
+
+9.Question 9
+What character do you add to the "+" or "*" to indicate that the match is to be done in a non-greedy manner?
+
+(a) ++
+(b) \g
+(c) $
+(d) **
+(f) ?
+(g) ^
+
+Ans :- ?
+
+10.Question 10
+Given the following line of text:
+
+(a) 
+(b) What would the regular expression '\S+?@\S+' match?
+(c) d@uct.ac.za
+(d) \@\
+(f) stephen.marquard@uct.ac.za
+(g) marquard@uct
+(h) From
+
+Ans :- stephen.marquard@uct.ac.za
