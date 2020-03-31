139c603a28273266a5c06f7ab35a84911af41724
AbhijeetFadol <46525845+Abhifadol@users.noreply.github.com>
2020-03-31 11:25:09 +0530

    Create Week 3 Quiz.py

---
+++ Week 3 Quiz.py
@@ -0,0 +1,110 @@
+##Networks and Sockets
+
+1.Question 1
+What do we call it when a browser uses the HTTP protocol to load a file or page from a server and display it in the browser?
+
+(a) Internet Protocol (IP)
+(b) DECNET
+(c) SMTP
+(d) IMAP
+(e) The Request/Response Cycle
+
+Ans :- The Request/Response Cycle
+
+2.Question 2
+Which of the following is most similar to a TCP port number?
+
+(a) A telephone extension
+(b) A street number in an address
+(c) The GPS coordinates of a building
+(d) The distance between two locations
+(e) A telephone number
+
+Ans :- A street number in an address...........(NOT CONFIRMED)
+
+3.Question 3
+What must you do in Python before opening a socket?
+
+(a) _socket = true
+(b) import tcp-socket
+(c) import tcp
+(d) open socket
+(e) import socket
+
+Ans :- import socket
+
+4.Question 4
+Which of the following TCP sockets is most commonly used for the web protocol (HTTP)?
+
+(a) 25
+(b) 22
+(c) 80
+(d) 23
+(e) 119
+
+Ans :- 80
+
+5.Question 5
+Which of the following is most like an open socket in an application?
+
+(a) An "in-progress" phone conversation
+(b) Fiber optic cables
+(c) The wheels on an automobile
+(d) The chain on a bicycle
+(e) The ringer on a telephone
+
+Ans :- An "in-progress" phone conversation
+
+6.Question 6
+What does the "H" of HTTP stand for?
+
+(a) Hyperspeed
+(b) HyperText
+(c) Manual
+(d) wHolsitic
+(e) Simple
+
+Ans :- HyperText
+
+7.Question 7
+What is an important aspect of an Application Layer protocol like HTTP?
+
+(a) How long do we wait before packets are retransmitted?
+(b) How much memory does the server need to serve requests?
+(c) Which application talks first? The client or server?
+(d) What is the IP address for a domain like www.dr-chuck.com?
+
+Ans :- Which application talks first? The client or server?
+
+8.Question 8
+What are the three parts of this URL (Uniform Resource Locator)?
+
+(a) Protocol, host, and document
+(b) Protocol, document, and offset
+(c) Page, offset, and count
+(d) Document, page, and protocol
+(e) Host, offset, and page
+
+Ans :- Protocol, host, and document
+
+9.Question 9
+When you click on an anchor tag in a web page like below, what HTTP request is sent to the server?
+
+(a) GET
+(b) POST
+(c) PUT
+(e) DELETE
+(f) INFO
+
+Ans :- GET
+
+10.Question 10
+Which organization publishes Internet Protocol Standards?
+
+(a) SIFA
+(b) LDAP
+(c) IETF
+(e) SCORM
+(f) IMS
+
+Ans :- IETF
