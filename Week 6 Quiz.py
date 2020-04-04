7e4a610c356a68ead6f819917c71e59ef44e4685
AbhijeetFadol <46525845+Abhifadol@users.noreply.github.com>
2020-04-04 22:10:04 +0530

    Create Week 6 Quiz.py

---
+++ Week 6 Quiz.py
@@ -0,0 +1,107 @@
+
+##REST, JSON, and APIs
+TOTAL POINTS 10
+
+1.Question 1
+Who is credited with the REST approach to web services?
+
+(a) Bjarne Stroustrup
+(b) Vint Cerf
+(c) Leonard Klienrock
+(d) Daphne Koller
+(e) Roy Fielding
+
+Ans :- Roy Fielding
+
+2.Question 2
+What Python library do you have to import to parse and handle JSON?
+
+(a) import re
+(b) ElementTree
+(c) import json
+(d) BeautifulSoup
+
+Ans :- import json
+
+3.Question 3
+Which of the following is a web services approach used by the Twitter API?
+
+(a) CORBA
+(b) SOAP
+(c) XML-RPC
+(d) REST
+
+Ans :- REST
+
+4.Question 4
+What kind of variable will you get in Python when the following JSON is parsed:
+[ "Glenn", "Sally", "Jen" ]
+
+(a) A list with three items
+(b) A dictionary with three key / value pairs
+(c) A dictionary with one key / value pair
+(d) Three tuples
+(e) One Tuple
+
+Ans :- A list with three items
+
+5.Question 5
+Which of the following is not true about the service-oriented approach?
+
+(a) Standards are developed where many pairs of applications must work together
+(b) An application runs together all in one place
+(c) Web services and APIs are used to transfer data between applications
+(d) An application makes use of the services provided by other applications
+
+Ans :- 
+
+6.Question 6
+Which of these two web service approaches is preferred in most modern service-oriented applications?
+
+(a) SOAP - Simple Object Access Protocol
+(b) REST - Representational state transfer
+
+Ans :- REST - Representational state transfer
+
+7.Question 7
+What library call do you make to append properly encoded parameters to the end of a URL like the following:
+http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=Ann+Arbor%2C+MI
+
+(a) re.match()
+(b) urllib.urlcat()
+(c) urllib.parse.urlencode()
+(d) re.encode()
+
+Ans :- urllib.parse.urlencode()
+
+8.Question 8
+What happens when you exceed the Google geocoding API rate limit?
+
+(a) You canot use the API until you respond to an email that contains a survey question
+(b) Your application starts to perform very slowly
+(c) The API starts to perform very slowly
+(d) You cannot use the API for 24 hours
+
+Ans :- You cannot use the API for 24 hours
+
+9.Question 9
+What protocol does Twitter use to protect its API?
+
+(a) Java Web Tokens
+(b) WS*Security
+(c) SOAP
+(d) PKI-HMAC
+(e) OAuth
+(f) SHA1-MD5
+
+Ans :- OAuth
+
+10.Question 10
+What header does Twitter use to tell you how many more API requests you can make before you will be rate limited?
+
+(a) content-type
+(b) x-request-count-down
+(c) x-rate-limit-remaining
+(d) x-max-requests
+
+Ans :- 
